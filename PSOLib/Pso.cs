52f5acca4b8da0bb72bf7cb0dbf9998040316e11
meszolym <marcell.meszoly@icloud.com>
2025-03-31 10:40:03 +0200

    refactor: reorganize project structure and enhance particle classes with additional features

---
+++ PSOLib/Pso.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics;
+using System.Timers;
+using HKDXX6_PSO.PSOLib.Models;
+
+namespace HKDXX6_PSO.PSOLib;
+
+public class Pso
+{
+    public ParticleSwarm Swarm { get; init; }
+    private readonly double _wOwn;
+    private readonly double _wSwarm;
+    private readonly Func<double[], double> _fitnessFunction;
+    private readonly Stopwatch _stopwatch = new();
+    private TimeSpan _maxTime;
+    
+    public Pso(int dimensions, int numberOfParticles, Func<double[], double> fitnessFunction, double wOwn, double wSwarm, Random? random = null)
+    {
+        random ??= Random.Shared;
+        
+        Swarm = new ParticleSwarm(dimensions);
+        _fitnessFunction = fitnessFunction;
+        _wOwn = wOwn;
+        _wSwarm = wSwarm;
+
+        for (var i = 0; i < numberOfParticles; i++)
+        {
+            Swarm.Particles.Add(Particle.FromRandom(dimensions, random));
+        }
+        Swarm.GlobalBestPosition = Swarm.Particles[0].BestPosition;
+        
+        
+    }
+
+    public void Run(TimeSpan maxTime)
+    {
+        _maxTime = maxTime;
+        _stopwatch.Start();   
+        while (!StopCondition())
+        {
+            Swarm.MoveAll();
+            Swarm.UpdateBestPositions(_fitnessFunction);
+            Swarm.UpdateVelocities(_wOwn, _wSwarm);
+        }
+        _stopwatch.Stop();
+        _stopwatch.Reset();
+    }
+
+    private bool StopCondition() => _fitnessFunction(Swarm.GlobalBestPosition) < 0.01 || _stopwatch.Elapsed > _maxTime;
+}
